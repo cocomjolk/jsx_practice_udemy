34781bdbad1d0a182eaac08ae0dba51b4f937c4f
Michael Quiroz <cocomjolk@hotmail.com>
2019-04-26 22:13:51 -0500

    first component rendered

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import react and reactDOM
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentDetail from './CommentDetail'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create react component
+// const App = function(){
+//   return <div>Hi there!</div>
+// }
+const App = () => {
+  return (
+    <div className="ui container comments" >
+      <CommentDetail />
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//take the react component and show it on the screen
+ReactDOM.render( <App />, document.querySelector('#root') )
